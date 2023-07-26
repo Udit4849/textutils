3ddcf4ded2be23c3b6df811be818f1c1815d8b6d
Udit4849 <105376419+Udit4849@users.noreply.github.com>
2023-07-26 18:57:06 +0530

    Update textform.js

--- src/components/textform.js
+++ src/components/textform.js
@@ -2,26 +2,22 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = () =>{
-        // console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText)
         props.showalert("Converted to uppercase!" , "success")
     }
     const handleLoClick = () =>{
-        // console.log("Uppercase was clicked" + text);
         let newText = text.toLowerCase();
         setText(newText)
         props.showalert("Converted to lowercase!" , "success")
 
     }
     const handleClearClick = () =>{
-        // console.log("Uppercase was clicked" + text);
         let newText = "";
         setText(newText)
     }
 
     const handleOnChange = (event) =>{
-        // console.log("on change");
         setText(event.target.value);
     }
     const handleCopy = ()=>{
@@ -35,8 +31,7 @@ export default function TextForm(props) {
     }
 
     const[text,setText] = useState('Enter text here');
-   // text = "newText"             // wrong way to change the state
-   // setText("newText")          // right way to cahnge the state
+   
   return (
     <>
     <div className="container" style={{color: props.mode==='dark'? 'white' : 'black'}}>
