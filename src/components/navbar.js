cfbb87665794ab88e432d864e5fae145d7963a8b
Udit4849 <105376419+Udit4849@users.noreply.github.com>
2023-07-26 18:51:44 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
@@ -13,17 +12,9 @@ export default function Navbar(props) {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item ">
-      {/* <Link className="nav-link active" to="/">Home</Link> */}
       <a className="nav-link active" href="#">Home</a>
       </li>
-      {/* <li className="nav-item">
-        <Link className="nav-link" to="/About">{props.abouttext}</Link>
-      </li> */}
     </ul>
-    {/* <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form> */}
     <div className={`form-check form-switch text-${props.mode==='light'? 'dark' : 'light'}`}>
       <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
       <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
