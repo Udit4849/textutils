fa709beadafcba53e54bab779d7df5ed3cb8afce
Udit4849 <105376419+Udit4849@users.noreply.github.com>
2024-05-17 17:38:50 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
@@ -12,8 +13,11 @@ export default function Navbar(props) {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item ">
-      <a className="nav-link active" href="#">Home</a>
+      <a className="nav-link active" href="/">Home</a>
       </li>
+       <li className="nav-item">
+        <Link className="nav-link" to="/About">{props.abouttext}</Link>
+      </li> 
     </ul>
     <div className={`form-check form-switch text-${props.mode==='light'? 'dark' : 'light'}`}>
       <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
